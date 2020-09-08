3107b2586f4070b3d8e70f663b0ace42a4737c59
Alexey Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2020-09-08 17:57:37 +0300

    0.0.1

---
+++ lab1_1/lab1_1/Graph.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <map>
+#include <set>
+
+using namespace std;
+
+template <typename T>
+class Graph
+{
+private:
+	class Edge;
+	class Top
+	{
+	public:
+		Top() {};
+		Top(T data)
+		{
+			this->data = data;
+		}
+		T data;
+		set <Edge*> links;
+	};
+	class Edge
+	{
+	public:
+		/*Edge() {};
+		Edge(T data) */
+		int weight;
+		set <Top*> links;
+	};
+	map<int, Top> graph_map;
+	int counter = 0;
+	
+public:
+	void add_top()
+	{
+		graph_map[counter];
+		counter++;
+	}
+	void add_top(T data)
+	{
+		graph_map[counter] = Top(data);
+		counter++;
+	}
+};
